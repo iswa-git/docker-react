3dc89495aef21c41b8e1a12f6908ee721dcc183c
MDev <ibrikisw@gmail.com>
2023-01-22 13:58:29 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,6 +8,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br />
+          Changed. Hello there. How are you? Test.
+          <br />1
         </p>
         <a
           className="App-link"
